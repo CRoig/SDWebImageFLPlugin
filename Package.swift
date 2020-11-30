713b02574610d3bbe91648a0ef28c122ecf6de42
Carles Roig <carles.roig@betvictor.com>
2020-11-30 15:51:15 +0100

    Update SDWebImage dependency version

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["SDWebImageFLPlugin"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0"),
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.10.0"),
         .package(url: "https://github.com/YetAnotherRzmn/FLAnimatedImage.git", from: "1.0.14")
     ],
     targets: [
